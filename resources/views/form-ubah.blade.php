c71d5f3550593f44a5ef42d40d1d76be011c8d11
sofanjitok <sofanjitok@gmail.com>
2021-04-21 17:28:31 +0700

    deploy lagi

--- resources/views/form-ubah.blade.php
+++ resources/views/form-ubah.blade.php
@@ -14,22 +14,22 @@ Ubah Kendaraan
     }
 </style>
 <div class="container">
-<form action="/ubah" method="POST" >
-<div class="form-group">
-        <label for="nopol">No. Polisi : </label>
-        <input class="form-control" type="text" name="nopol" id="nopol" value="{{ $data[0]->nopol }}">
-    </div>
-    <div class="form-group">
-        <label for="merk">Merk : </label>
-        <input class="form-control" type="text" name="merk" id="merk" value="{{ $data[0]->merk }}">
-    </div>
-    <div class="form-group">
-        <label for="tipe">Tipe : </label>
-        <input class="form-control" type="text" name="tipe" id="tipe" value="{{ $data[0]->tipe }}">
-    </div>
-    <input type="hidden" value="{{ $data[0]->id }}" id="id" name="id">
-    <input class="btn float-right btn-success" type="submit" value="Simpan"><br>
-</form>
+    <form action="/ubah" method="POST">
+        <div class="form-group">
+            <label for="nopol">Nomor Kendaraan : </label>
+            <input class="form-control" type="text" name="nopol" id="nopol" value="{{ $data[0]->nopol }}">
+        </div>
+        <div class="form-group">
+            <label for="merk">Merk Kendaraan: </label>
+            <input class="form-control" type="text" name="merk" id="merk" value="{{ $data[0]->merk }}">
+        </div>
+        <div class="form-group">
+            <label for="tipe">Tipe Kendaraan: </label>
+            <input class="form-control" type="text" name="tipe" id="tipe" value="{{ $data[0]->tipe }}">
+        </div>
+        <input type="hidden" value="{{ $data[0]->id }}" id="id" name="id">
+        <input class="btn float-right btn-success" type="submit" value="Simpan"><br>
+    </form>
 </div>
 
 @endsection
--- resources/views/tambah-kendaraan.blade.php
+++ resources/views/tambah-kendaraan.blade.php
@@ -14,23 +14,23 @@ Tambah Kendaraan
     }
 </style>
 <div class="container">
-<form action="/simpan-kendaraan" method="POST" >
-    <div class="form-group">
-        <label for="nopol">No. Polisi : </label>
-        <input class="form-control" type="text" name="nopol" id="nopol" required>
-    </div>
-    <div class="form-group">
-        <label for="merk">Merk : </label>
-        <input class="form-control" type="text" name="merk" id="merk" required>
-    </div>
-    <div class="form-group">
-        <label for="tipe">Tipe : </label>
-        <input class="form-control" type="text" name="tipe" id="tipe" required>
-    </div>
-    <input class="btn btn-success float-right" type="submit" value="Simpan"><br>
+    <form action="/simpan-kendaraan" method="POST">
+        <div class="form-group">
+            <label for="nopol">Nomor Kendaraan : </label>
+            <input class="form-control" type="text" name="nopol" id="nopol" required>
+        </div>
+        <div class="form-group">
+            <label for="merk">Merk Kendaraan: </label>
+            <input class="form-control" type="text" name="merk" id="merk" required>
+        </div>
+        <div class="form-group">
+            <label for="tipe">Tipe Kendaraan : </label>
+            <input class="form-control" type="text" name="tipe" id="tipe" required>
+        </div>
+        <input class="btn btn-success float-right" type="submit" value="Simpan"><br>
 
-    
-</form>
+
+    </form>
 </div>
 
 
